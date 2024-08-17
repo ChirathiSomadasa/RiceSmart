7f713f1b7f9726eb3a641a39c8acb9ab5f42eb21
Chirathi Somadasa <chirathisomadasa@gmail.com>
2024-08-17 21:42:11 +0530

    Create UI for Sign Up page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/header/Header';
+import SignUp from './pages/signup/SignUp';
 
 function App() {
   return (
     <BrowserRouter>
     <Header />
     <Routes>
-      
+    <Route path="/signup" element={<SignUp />} />
     </Routes>
   </BrowserRouter>
   );
